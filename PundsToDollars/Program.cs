e723bfd2b758e2f106129fd4fe06d76c50184cb9
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 08:46:29 +0300

    convertor from puond

---
+++ PundsToDollars/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PundsToDollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"{pounds * 1.31m:F3}");
+        }
+    }
+}
