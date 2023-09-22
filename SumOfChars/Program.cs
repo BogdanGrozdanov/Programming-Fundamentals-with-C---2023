9883ab7b2639de25be417c66bd3cf1eb22abc8a3
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 13:51:07 +0300

    Update Program.cs

--- SumOfChars/Program.cs
+++ SumOfChars/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < n; i++) { char letter = char.Parse(Console.ReadLine()); sum += letter; }
+            Console.WriteLine($"The sum equals: {sum}");
         }
     }
 }
