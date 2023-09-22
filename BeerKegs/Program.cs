dd37c1bc756dc10217af38a733c59395e172e460
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 16:25:01 +0300

    Create Program.cs

---
+++ BeerKegs/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double biggest = 0;
+            string biggestName = String.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+                if (volume > biggest) { biggest = volume; biggestName = model; }
+            }
+            Console.WriteLine(biggestName);
+
+        }
+    }
+}
