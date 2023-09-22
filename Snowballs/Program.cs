c8f2c73fa9f29acf2d36c574e2ca6d413ac481c3
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 18:16:28 +0300

    Update Program.cs

--- Snowballs/Program.cs
+++ Snowballs/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Snowballs
+﻿using System.Numerics;
+
+namespace Snowballs
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfSnowball = int.Parse(Console.ReadLine());
+            BigInteger snowballValue = 0;
+            int highestSnowballSnow = 0;
+            int highestSnowballTime = 0;
+            int highestSnowballQuality = 0;
+
+            for (int i = 0; i < numberOfSnowball; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+                BigInteger snowballValueM = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+
+                if (snowballValue < snowballValueM) { snowballValue = snowballValueM; highestSnowballSnow = snowballSnow; highestSnowballTime = snowballTime; highestSnowballQuality = snowballQuality; }
+            }
+            Console.WriteLine($"{highestSnowballSnow} : {highestSnowballTime} = {snowballValue} ({highestSnowballQuality})");
         }
     }
 }
