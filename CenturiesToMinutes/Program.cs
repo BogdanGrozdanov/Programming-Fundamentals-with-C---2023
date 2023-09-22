cb28ca1051c4557e116af1f81958d30877c949e7
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 09:48:34 +0300

    add project

--- CenturiesToMinutes/Program.cs
+++ CenturiesToMinutes/Program.cs
@@ -4,9 +4,12 @@
     {
         static void Main(string[] args)
         {
-            double centuries = int.Parse(Console.ReadLine());
-            string result = $"{centuries} centuries = {centuries * 100} years = {centuries * 36524.22} days = {centuries * 876576} hours = {52594560 * centuries} minutes";
-            Console.WriteLine(result);
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days =(int)( years * 365.2422);
+            long hours =(long) days * 24;
+            long minutes =(long) hours * 60;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days:f0} days = {hours} hours = {minutes} minutes");
         }
     }
 }
---
+++ SpecialNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SpecialNumbers
+{
+    /* A number is special when its sum of digits is 5, 7 or 11.
+
+
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            bool isSpesial = true;
+            Console.WriteLine(isSpesial);
+            for (int i = 1; i <= n; i++)
+            {
+            
+            }
+        }
+    }
+}
