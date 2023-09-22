e6a243170239804c5ac2f7088162afb1b134d37a
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 09:24:03 +0300

    Update Program.cs

--- CenturiesToMinutes/Program.cs
+++ CenturiesToMinutes/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double centuries = int.Parse(Console.ReadLine());
+            string result = $"{centuries} centuries = {centuries * 100} years = {centuries * 36524.22} days = {centuries * 876576} hours = {52594560 * centuries} minutes";
+            Console.WriteLine(result);
         }
     }
 }
