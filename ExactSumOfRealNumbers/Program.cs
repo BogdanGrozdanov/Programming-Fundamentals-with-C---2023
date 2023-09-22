9a6d17e4f0b22169dfd680b817ba990a48bb6386
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 09:11:53 +0300

    Update Program.cs

--- ExactSumOfRealNumbers/Program.cs
+++ ExactSumOfRealNumbers/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ExactSumOfRealNumbers
+﻿using System.Diagnostics.Metrics;
+using System.Threading.Channels;
+
+namespace ExactSumOfRealNumbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            decimal totalSum = 0;
+
+            while (n != counter) { counter++; decimal number = decimal.Parse(Console.ReadLine()); totalSum += number; }
+            Console.WriteLine(totalSum);
         }
     }
 }
