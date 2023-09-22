09822bc301de2c80e48aea62e5683c803c07dac6
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 13:22:08 +0300

    Update Program.cs

--- IntegerOperations/Program.cs
+++ IntegerOperations/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+
+            long firstCondition = num1 + num2;
+            long secondCondition = firstCondition / num3;
+            long thirdCondition = secondCondition * num4;
+            Console.WriteLine(thirdCondition);
         }
     }
 }
