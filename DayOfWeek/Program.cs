ef199cfcc913b301b692eed127d014ede3ed3e01
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 19:52:12 +0300

    Update Program.cs

--- DayOfWeek/Program.cs
+++ DayOfWeek/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] dayOfWeek = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0 || num > dayOfWeek.Length) { Console.WriteLine("Invalid day"); }
+            else { Console.WriteLine(dayOfWeek[num - 1]); }
         }
     }
 }
