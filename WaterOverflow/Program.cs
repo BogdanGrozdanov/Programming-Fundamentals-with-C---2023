573f047a09a5e8e1295fc00d411e065bf3dec094
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 16:09:41 +0300

    Create Program.cs

---
+++ WaterOverflow/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int capacity = 255;
+            int sumWater = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int water = int.Parse(Console.ReadLine());
+                sumWater += water;
+
+                if (capacity < sumWater) { Console.WriteLine("Insufficient capacity!"); sumWater -= water; }
+            }
+            Console.WriteLine(sumWater);
+        }
+    }
+}
