447d748930763526526011b960706efe4e90fcc3
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 14:58:05 +0300

    Create Program.cs

---
+++ TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TriplesOfLatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int firstLetter = 97;
+                int lastLeter =firstLetter + n;
+            for (int i = firstLetter; i < lastLeter; i++)
+            {
+                char firstChar = (char)i;
+
+                for (int j = firstLetter; j < lastLeter; j++) 
+                {
+                    char secondChar = (char)j;
+
+                    for (int k = firstLetter; k < lastLeter; k++)
+                    {
+                        char thirthChar = (char)k;
+                        Console.WriteLine($"{firstChar}{secondChar}{thirthChar}");
+                    }
+                }
+            }
+        }
+    }
+}
