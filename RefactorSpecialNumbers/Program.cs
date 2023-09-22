fdb9d195eb24229cf6b9628cd1c5ac2dc30c53a5
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 12:42:25 +0300

    Update Program.cs

--- RefactorSpecialNumbers/Program.cs
+++ RefactorSpecialNumbers/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = i;
+                int sum = 0;
+
+                while (number > 0)
+                {
+                    int lastDigit = number % 10;
+                    sum += lastDigit;
+                    number /= 10;
+                }
+                bool isspecial = sum == 5 || sum == 7 || sum == 11;
+                Console.WriteLine($"{i} -> {isspecial}");
+
+            }
         }
     }
 }
