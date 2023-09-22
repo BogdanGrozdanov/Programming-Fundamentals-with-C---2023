19c4750902b898aafe18f811a498659173ed9070
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 13:32:50 +0300

    Update Program.cs

--- SumDigits/Program.cs
+++ SumDigits/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string number = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                sum += int.Parse(number[i].ToString());
+            }
+            Console.WriteLine(sum);
         }
     }
 }
