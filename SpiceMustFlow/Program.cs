806e171156a3851e24393824e090aaa4715a4580
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 17:19:58 +0300

    yield

---
+++ SpiceMustFlow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int days = 0;
+            int totalYield = 0;
+
+            while (startingYield > 99)
+            {
+                days++;
+                totalYield += startingYield;
+                totalYield -= 26;
+                startingYield -= 10;
+            }
+            totalYield -= 26;
+            if (totalYield < 0) { totalYield = 0; }
+            Console.WriteLine(days);
+            Console.WriteLine(totalYield);
+        }
+    }
+}
