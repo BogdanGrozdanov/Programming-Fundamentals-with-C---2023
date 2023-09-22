c0312ea42bd9b54e79dd42b4c804e2a7c7ff6776
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-22 13:42:02 +0300

    add more :)

---
+++ Elevator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            double courses = numOfPeople / (double)capacity;
+            Console.WriteLine(Math.Ceiling(courses));
+        }
+    }
+}
