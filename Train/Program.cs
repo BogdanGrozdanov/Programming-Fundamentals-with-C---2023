9cf5bfa01e84adb5d3f9042d7d2743a758a15ad1
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-23 18:48:37 +0300

    Update Program.cs

--- Train/Program.cs
+++ Train/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello,  World!");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int i in arr) { Console.Write(i + " "); }
+            Console.WriteLine();
+            Console.WriteLine(arr.Sum());
         }
     }
 }
