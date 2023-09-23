873492f24c84d573ce9316ced18669d76c9cbaec
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-23 20:08:39 +0300

    Create Program.cs

---
+++ CommonElements/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine().Split().ToArray();
+            string[] secondArr = Console.ReadLine().Split().ToArray();
+
+            string[] commonArr = new string[firstArr.Length];
+
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                for (int j = 0; j < secondArr.Length; j++)
+                {
+                    if (firstArr[i] == secondArr[j])
+                    {
+                        commonArr[i] = firstArr[i];
+                    }
+                }
+            }
+            for (int i = commonArr.Length - 1; i >= 0; i--)
+            { Console.Write(commonArr[i] + " "); }
+        }
+    }
+}
