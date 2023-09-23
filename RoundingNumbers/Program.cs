c5ed83ec07ebe144c304276d0ab74144e710505c
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-23 15:39:42 +0300

    Create Program.cs

---
+++ RoundingNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == -0) 
+                { arr[i] = 0; }
+
+                Console.WriteLine($"{arr[i]} => {Math.Round(arr[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
