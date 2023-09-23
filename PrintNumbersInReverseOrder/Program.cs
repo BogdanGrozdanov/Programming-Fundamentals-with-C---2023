588ce7eb0e70557d03d37098c8d9a65a9fefbb1d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-23 15:14:48 +0300

    Update Program.cs

--- PrintNumbersInReverseOrder/Program.cs
+++ PrintNumbersInReverseOrder/Program.cs
@@ -4,14 +4,16 @@
     {
         static void Main(string[] args)
         {
-           int n =int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
             int[] arr = new int[n];
-            for (int i = 0; i < n; i++) 
+            for (int i = 0; i < n; i++)
             {
                 arr[i] = int.Parse(Console.ReadLine());
             }
-
-            //foreach (int i in arr) { Console.WriteLine(i); }
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
         }
     }
 }
