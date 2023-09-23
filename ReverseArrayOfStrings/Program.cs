df7aa83fb2b8bb1fb9dc621e2f82815510d7d463
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-23 16:24:49 +0300

    Create Program.cs

---
+++ ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split(" ");
+            for (int i = strings.Length - 1; i >= 0; i--)
+            { Console.Write(strings[i] + " "); }
+        }
+    }
+}
