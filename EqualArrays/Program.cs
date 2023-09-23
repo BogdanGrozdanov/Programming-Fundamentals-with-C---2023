ef60127ac2e13910d45e3ef86ca1f9a6d9ef2189
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-23 17:52:13 +0300

    add Equal arrays

---
+++ EqualArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+            int index = 0;
+            bool isEqual = default;
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+
+                if (firstArr[i] != secondArr[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {index} index");
+                    isEqual = true;
+                    break;
+                }
+                index++;
+                sum += firstArr[i];
+            }
+            if (!isEqual) { Console.WriteLine($"Arrays are identical. Sum: {sum}"); }
+        }
+    }
+}
