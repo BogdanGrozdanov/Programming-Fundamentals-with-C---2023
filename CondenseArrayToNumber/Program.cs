22ec23a78ce6af8323e3051a73030ac2972f7d80
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-23 18:25:25 +0300

    Update Program.cs

--- CondenseArrayToNumber/Program.cs
+++ CondenseArrayToNumber/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arrInput = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            if (arrInput.Length == 1)
+            {
+                Console.WriteLine(arrInput[0]);
+                return;
+            }
+            else
+            {
+                while (arrInput.Length != 1)
+                {
+                    int[] condensed = new int[arrInput.Length - 1];
+                    for (int i = 0; i < arrInput.Length - 1; i++)
+                    {
+                        condensed[i] = arrInput[i] + arrInput[i + 1];
+                    }
+                    arrInput = condensed;
+                }
+            }
+
+            Console.WriteLine(arrInput[0]);
         }
     }
 }
