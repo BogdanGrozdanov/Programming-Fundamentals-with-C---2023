e364898de4b0032e9fcf3f71b642e9062dd1e12b
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-25 11:54:24 +0300

    Update Program.cs

--- ReverseArrayOfStrings/Program.cs
+++ ReverseArrayOfStrings/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            // Console.WriteLine(string.Join(" ", Console.ReadLine()).Split().Reverse());
             string[] strings = Console.ReadLine().Split(" ");
             for (int i = strings.Length - 1; i >= 0; i--)
             { Console.Write(strings[i] + " "); }
