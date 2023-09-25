837859343c38bf98a39ea935de897902066d3f05
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-25 11:54:22 +0300

    Create Program.cs

---
+++ Zig-ZagArrays/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] muber = { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+            foreach (int i in muber) { Console.WriteLine(i); }
+        }
+    }
+}
