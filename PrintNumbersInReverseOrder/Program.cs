0daa676449841f740f97824191378cc9d0b668ff
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-26 12:25:57 +0300

    Update Program.cs

--- PrintNumbersInReverseOrder/Program.cs
+++ PrintNumbersInReverseOrder/Program.cs
@@ -10,10 +10,7 @@
             {
                 arr[i] = int.Parse(Console.ReadLine());
             }
-            for (int i = arr.Length - 1; i >= 0; i--)
-            {
-                Console.Write(arr[i] + " ");
-            }
+            Console.WriteLine(string.Join(' ', arr.Reverse()));
         }
     }
 }
