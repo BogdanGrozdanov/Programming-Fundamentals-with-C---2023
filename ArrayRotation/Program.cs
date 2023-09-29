06d299da54dcfc1e6bf1607f850554c168944b7e
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-29 20:28:11 +0300

    Create Program.cs

---
+++ ArrayRotation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int rotationCount = int.Parse(Console.ReadLine());
+            string[] curentArr = new string[arr.Length];
+
+            for (int i = 0; i < rotationCount; i++)
+            {
+                curentArr[i] = arr[arr.Length -1];
+                for (int j = 0; j < arr.Length; j++) { arr[j]}
+                arr[i] = curentArr[i];
+            }
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
