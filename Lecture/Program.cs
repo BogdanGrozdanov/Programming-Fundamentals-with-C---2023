43bd248f1a0a3f746ff1d05da55f60aa33434022
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-09-30 18:35:41 +0300

    Create Program.cs

---
+++ Lecture/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrNumber = new int[] { 1, 2, 3, 4 };
+            //foreach (int i in arrNumber) { Console.Write(i +" ") ; }
+
+
+            Console.WriteLine(arrNumber.Length);
+            Console.WriteLine(arrNumber[3] + 2);
+
+        }
+    }
+}
