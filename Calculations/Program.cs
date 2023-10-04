4ef15139d359b1c60a30cf5acb0b84c4f58ade9f
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-04 19:30:54 +0300

    Update Program.cs

--- Calculations/Program.cs
+++ Calculations/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Calculations
+﻿using System.ComponentModel;
+
+namespace Calculations
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string command = Console.ReadLine();
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            switch (command)
+            {
+                case "add":
+                    Add(a, b);
+                    break;
+                case "multiply":
+                    Multiply(a, b);
+                    break;
+                case "subtract":
+                    Substract(a, b);
+                    break;
+                case "divide":
+                    Divide(a, b);
+                    break;
+            }
+
+        }
+
+        static void Divide(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+
+        static void Substract(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+        static void Multiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
         }
     }
 }
