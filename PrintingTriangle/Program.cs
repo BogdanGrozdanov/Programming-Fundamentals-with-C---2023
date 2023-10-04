0b5ac9caefcbb0f75b5ebca6db89f3c96a85da0d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-04 19:30:57 +0300

    Update Program.cs

--- PrintingTriangle/Program.cs
+++ PrintingTriangle/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace PrintingTriangle
+﻿
+namespace PrintingTriangle
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+            PrintingTriangle(number);
+        }
+
+        static void PrintingTriangle(int number)
+        {
+            for (int i = 1; i <= number; i++) 
+            { Console.WriteLine(i);
+                
+            }
         }
     }
 }
