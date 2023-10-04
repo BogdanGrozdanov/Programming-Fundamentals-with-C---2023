324d5f327a0aadd7ea2b88a7f2d834771951dab5
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-04 18:18:51 +0300

    Create Program.cs

---
+++ TopIntegers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] topInts = new int[ints.Length];
+            int i = 0;
+
+        }
+    }
+}
