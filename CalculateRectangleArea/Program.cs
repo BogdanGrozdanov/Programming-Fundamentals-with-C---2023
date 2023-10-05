d5e157d15e9044ba1e0ac05dc1dd3656f3a63527
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-05 18:12:06 +0300

    add CalculateRectangleArea

---
+++ CalculateRectangleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int area = CalculateRectangleArea(a, b);
+            Console.WriteLine(area);
+        }
+
+        private static int CalculateRectangleArea(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
