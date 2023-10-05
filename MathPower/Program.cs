39a782d3b78e2c4c7c9a358311655ddd1d758b36
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-05 18:47:07 +0300

    Create Program.cs

---
+++ MathPower/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = Math.Pow(baseNumber, power);
+            Console.WriteLine(result);
+        }
+    }
+}
