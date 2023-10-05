f6c10686ae7c15d3fae454f71ed86f88ff389479
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-05 18:25:07 +0300

    add repeat string

---
+++ RepeatString/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringa = Console.ReadLine();
+            string repeat = Console.ReadLine();
+            string result = RepeatStrings(stringa, repeat);
+            Console.WriteLine(result);
+        }
+
+        static string RepeatStrings(string stringa, string repeat)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < int.Parse(repeat); i++) { result += stringa; };
+            return result;
+        }
+    }
+}
