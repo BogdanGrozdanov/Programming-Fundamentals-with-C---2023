1bce5176ccefce4b90293c47a267f6829a605d32
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-05 18:08:02 +0300

    add OrdersMethod

--- OrdersMethods/Program.cs
+++ OrdersMethods/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace OrdersMethods
+﻿
+namespace OrdersMethods
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string orderType = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            Order(orderType, quantity);
+        }
+
+        static void Order(string orderType, int quantity)
+        {
+            if (orderType == "coffee") {; Console.WriteLine($"{1.5 * quantity:F2}"); }
+            else if (orderType == "water") { Console.WriteLine($"{1.00 * quantity:F2}"); }
+            else if (orderType == "coke") { Console.WriteLine($"{1.40 * quantity:F2}"); }
+            else if (orderType == "snacks") { Console.WriteLine($"{2.00 * quantity:F2}"); }
         }
     }
 }
