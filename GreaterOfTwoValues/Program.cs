71f0b0d86277914f7233756c3d2798bfaed0fcc6
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-06 11:57:02 +0300

    Update Program.cs

--- GreaterOfTwoValues/Program.cs
+++ GreaterOfTwoValues/Program.cs
@@ -11,16 +11,18 @@ namespace GreaterOfTwoValues
 
             if (type == "string") { Console.WriteLine(GetMax(a, b)); }
             else if (type == "int") { Console.WriteLine(GetMax(int.Parse(a), int.Parse(b))); }
-                
-            
-
+            else if (type == "char") { Console.WriteLine(GetMax(char.Parse(a), char.Parse(b))); }
         }
 
         static string GetMax(string a, string b)
         {
-            if (a.CompareTo(b) > 0) { return a; }else { return b; }
+            if (a.CompareTo(b) > 0) { return a; } else { return b; }
         }
         static int GetMax(int a, int b)
+        {
+            if (a.CompareTo(b) > 0) { return a; } else { return b; }
+        }
+        static char GetMax(char a, char b)
         {
             if (a.CompareTo(b) > 0) { return a; } else { return b; }
         }
