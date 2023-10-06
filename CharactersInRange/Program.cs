1451caddad9897771f15b17f8e0141ea941f0d17
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-06 13:52:49 +0300

    Update Program.cs

--- CharactersInRange/Program.cs
+++ CharactersInRange/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace CharactersInRange
+﻿
+namespace CharactersInRange
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char startChar = char.Parse(Console.ReadLine());
+            char endChar = char.Parse(Console.ReadLine());
+            PrintCharInRange(startChar, endChar);
+        }
+
+        private static void PrintCharInRange(char startChar, char endChar)
+        {
+            for (int i = startChar+1; i < endChar; i++)
+            {
+                char character = (char)i;
+                Console.Write(character +" ");
+            }
         }
     }
 }
