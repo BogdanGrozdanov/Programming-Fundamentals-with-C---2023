0270698003f2f1c072ce3fb6d296a27646211b18
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-06 13:34:12 +0300

    Create Program.cs

---
+++ SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstInt = int.Parse(Console.ReadLine());
+            int secondInt = int.Parse(Console.ReadLine());
+            int thirthInt = int.Parse(Console.ReadLine());
+            GetSmallestInteger(firstInt, secondInt, thirthInt);
+        }
+         static void GetSmallestInteger(int firstInt, int secondInt, int thirthInt)
+        {
+             int[] numbers = new int[] { firstInt, secondInt, thirthInt };
+            int min=numbers.Min();
+            Console.WriteLine(min);
+        }
+    }
+}
