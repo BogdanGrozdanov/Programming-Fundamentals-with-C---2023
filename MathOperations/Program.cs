4ec85d7a98388101fcecc5965665d19a87b52325
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-06 13:23:50 +0300

    Update Program.cs

--- MathOperations/Program.cs
+++ MathOperations/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double firstNumber = double.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            double secondNumber = double.Parse(Console.ReadLine());
+            Calculate(firstNumber, @operator, secondNumber);
+        }
+        static void Calculate(double firstNumber, string @operator, double secondNumber)
+        {
+            double result = 0;
+            switch (@operator)
+            {
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+            }
+            Console.WriteLine(result);
         }
     }
 }
