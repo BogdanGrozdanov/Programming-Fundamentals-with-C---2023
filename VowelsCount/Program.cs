8d736bba97c67f3a555699d8db2641af0b13b493
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-06 13:44:30 +0300

    Create Program.cs

---
+++ VowelsCount/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            CountVowels(input);
+        }
+
+        static void CountVowels(string input)
+        {
+            int counter = 0;
+            input = input.ToLower();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'y')
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+
+        }
+
+    }
+}
