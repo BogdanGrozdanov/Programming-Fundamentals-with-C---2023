d96c8f9567550731473f46d26e2a07b65dca4b47
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-08 12:17:24 +0300

    Update Program.cs

--- MiddleCharacters/Program.cs
+++ MiddleCharacters/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            string single =Console.ReadLine();
+            string single = Console.ReadLine();
+            MiddleCharacters(single);
+        }
+        static void MiddleCharacters(string singleString)
+        {
+            int startIndex = 0;
+            int endIndex = singleString.Length -1;
+
+            while (startIndex <= endIndex)
+            {
+                if (startIndex == endIndex)
+                {
+                    for (int i = startIndex; i <= endIndex; i++)
+                    {
+                        Console.Write(singleString[startIndex]);
+                    }
+                }
+                startIndex++;
+                endIndex--;
+            }
         }
     }
 }
