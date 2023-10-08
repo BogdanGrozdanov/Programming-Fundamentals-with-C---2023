4990d15c7f804753a675c91ce8da320029ea6bda
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-08 13:47:21 +0300

    Create Program.cs

---
+++ PalindromeIntegers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+
+                    for (int j = input.Length - 1; j >= 0; j--)
+                    {
+
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
