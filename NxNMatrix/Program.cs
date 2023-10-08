90d52de88e95b8a1da7d1d27ed8d83cacf29b148
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-08 12:45:54 +0300

    Create Program.cs

---
+++ NxNMatrix/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace NxNMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            NxNMatrix(n);
+        }
+        static void NxNMatrix(int n)
+        {
+            for (int row = 0; row < n; row++)
+            {
+                for(int col = 0; col < n; col++) 
+                {
+                    Console.Write($"{n}"+" ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
