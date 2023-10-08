5e1e142d6106ead47d63dc2625519c5da1566abe
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-08 12:54:54 +0300

    Create Program.cs

---
+++ FactorialDivision/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstInteger = int.Parse(Console.ReadLine());
+            int secondInteger = int.Parse(Console.ReadLine());
+
+            double result = Factoriel(firstInteger) / Factoriel(secondInteger);
+            Console.WriteLine($"{result:F2}");
+        }
+
+        static int Factoriel(int firstInteger)
+        {
+            int factoriel = 1;
+
+            for (int i = 1; i <= firstInteger; i++)
+            {
+                factoriel *= i;
+            }
+            return factoriel;
+        }
+    }
+}
