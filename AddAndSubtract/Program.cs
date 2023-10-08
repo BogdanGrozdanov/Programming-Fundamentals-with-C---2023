f9925eca896c08503ee3a76b2ed04fce14ba76d5
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-08 10:44:42 +0300

    Create Program.cs

---
+++ AddAndSubtract/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer1 = int.Parse(Console.ReadLine());
+            int integer2 = int.Parse(Console.ReadLine());
+            int integer3 = int.Parse(Console.ReadLine());
+            int addInteger = AddTwoIntegers(integer1, integer2);
+            int result = SubstractTwoInteger(addInteger, integer3);
+            Console.WriteLine(result);
+        }
+        static int AddTwoIntegers(int first, int second)
+        {
+            int result = first + second;
+            return result;
+        }
+        static int SubstractTwoInteger(int first, int second)
+        {
+            int result = first - second;
+            return result;
+        }
+    }
+}
