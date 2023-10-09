52c7be5d7bf4469291c0771daec483f0e32f7b44
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-09 10:45:06 +0300

    Create Program.cs

---
+++ TopNumber/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace TopNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTopInteger(n);
+        }
+
+        static void PrintTopInteger(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (IsTopNumber(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool IsTopNumber(int number)
+        {
+            if (IsDivisibleBy8(number) && ContainsOddDigit(number))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool IsDivisibleBy8(int number)
+        {
+            int sumOfDigits = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sumOfDigits += digit;
+                number /= 10;
+            }
+
+            return sumOfDigits % 8 == 0;
+        }
+
+        static bool ContainsOddDigit(int number)
+        {
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 != 0)
+                {
+                    return true;
+                }
+                number /= 10;
+            }
+
+            return false;
+        }
+    }
+}
