f8613375e4abf136fa672c680bcd1521273cef42
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-09 12:01:48 +0300

    Update Program.cs

--- CenterPoint/Program.cs
+++ CenterPoint/Program.cs
@@ -4,14 +4,18 @@
     {
         static void Main(string[] args)
         {
-            //d =
             double x1 = double.Parse(Console.ReadLine());
             double y1 = double.Parse(Console.ReadLine());
             double x2 = double.Parse(Console.ReadLine());
             double y2 = double.Parse(Console.ReadLine());
+            double firstPoint = PitagorTheoremDekartCoordinatSystem(x1, y1);
+            double secontPoint = PitagorTheoremDekartCoordinatSystem(x2, y2);
+            if (firstPoint > secontPoint) { Console.WriteLine($"({x2}, {y2})"); } else { Console.WriteLine($"({x1}, {y1})"); }
+        }
 
-            double firstPoint = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine(firstPoint);
+        private static double PitagorTheoremDekartCoordinatSystem(double x1, double y1)
+        {
+            return Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
         }
     }
 }
