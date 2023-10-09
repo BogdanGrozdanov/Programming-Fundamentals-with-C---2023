51eb77704443e508e1dbea55f66757e2a73eae7a
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-09 11:11:03 +0300

    more :)

---
+++ CenterPoint/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CenterPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DataType/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            switch (type)
+            {
+                case "int":
+                    MultiplayIntegerX2();
+                    break;
+                case "real":
+                    FormatToSecondDecimalPoint();
+                    break;
+                case "string":
+                    AddSymboltoSring();
+                    break;
+            }
+        }
+
+        static void AddSymboltoSring()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine($"${input}$");
+        }
+
+        static void FormatToSecondDecimalPoint()
+        {
+            double number = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{(number * 1.5):F2}");
+        }
+
+        static void MultiplayIntegerX2()
+        {
+            int number = int.Parse(Console.ReadLine());
+            number *= 2;
+            Console.WriteLine(number);
+        }
+    }
+}
