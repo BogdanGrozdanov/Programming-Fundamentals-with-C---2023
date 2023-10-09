e634c83147373bb028289433d9742b2d6aa23430
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-09 11:47:43 +0300

    Update Program.cs

--- CenterPoint/Program.cs
+++ CenterPoint/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //d =
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double firstPoint = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(firstPoint);
         }
     }
 }
