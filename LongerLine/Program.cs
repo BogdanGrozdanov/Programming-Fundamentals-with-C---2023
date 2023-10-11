2fb89e2900558e611648a6271facf7912b17f2aa
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-11 11:15:11 +0300

    Update Program.cs

--- LongerLine/Program.cs
+++ LongerLine/Program.cs
@@ -15,35 +15,30 @@ namespace LongerLine
             double x4 = double.Parse(Console.ReadLine());
             double y4 = double.Parse(Console.ReadLine());
 
-            double[] firstPoint = { x1, y1 };
-            double[] secondPoint = { x2, y2 };
-            double[] thirthPoint = { x3, y3 };
-            double[] fourPoint = { x4, y4 };
+            double l1 = PitagorTheoremDekartCoordinatSystem(x1, y1);
+            double l2 = PitagorTheoremDekartCoordinatSystem(x2, y2);
+            double l3 = PitagorTheoremDekartCoordinatSystem(x3, y3);
+            double l4 = PitagorTheoremDekartCoordinatSystem(x4, y4);
 
-            double l1 = PitagorTheoremDekartCoordinatSystem(firstPoint[0], firstPoint[1]);
-            double l2 = PitagorTheoremDekartCoordinatSystem(secondPoint[0], secondPoint[1]);
-            double l3 = PitagorTheoremDekartCoordinatSystem(thirthPoint[0], thirthPoint[1]);
-            double l4 = PitagorTheoremDekartCoordinatSystem(fourPoint[0], fourPoint[1]);
-
-            double[] arangedArrayByOrder = new double[3];
-            for (int i = 0; i < arangedArrayByOrder.Length; i++)
-            { 
-            
+            string result =string.Empty;
+            double max
+            if (l1 > l2)
+            {
+                Console.WriteLine($"({x2}, {y2})");
             }
-
-
-
-
+            else
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            if (l3 > l4)
+            {
+                Console.WriteLine($"({x4}, {y4})");
+            }
+            else
+            {
+                Console.WriteLine($"({x3}, {y3})");
+            }
         }
-
-
-
-        static double GetMax(double a, double b)
-        {
-            if (a.CompareTo(b) > 0) { return a; } else { return b; }
-        }
-
-
         static double PitagorTheoremDekartCoordinatSystem(double x1, double y1)
         {
             return Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
