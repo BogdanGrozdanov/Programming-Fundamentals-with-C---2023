3446619a414232e2737e613d7348f287fb9967f3
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-11 13:03:59 +0300

    Update Program.cs

--- LongerLine/Program.cs
+++ LongerLine/Program.cs
@@ -6,59 +6,67 @@ namespace LongerLine
     {
         static void Main(string[] args)
         {
-            double x1 = double.Parse(Console.ReadLine());
-            double y1 = double.Parse(Console.ReadLine());
-            double x2 = double.Parse(Console.ReadLine());
-            double y2 = double.Parse(Console.ReadLine());
-            double x3 = double.Parse(Console.ReadLine());
-            double y3 = double.Parse(Console.ReadLine());
-            double x4 = double.Parse(Console.ReadLine());
-            double y4 = double.Parse(Console.ReadLine());
+            double x1, y1, x2, y2, x3, y3, x4, y4;
+            ReadInput(out x1, out y1, out x2, out y2, out x3, out y3, out x4, out y4);
 
-            double l1 = PitagorTheoremDekartCoordinatSystem(x1, y1);
-            double l2 = PitagorTheoremDekartCoordinatSystem(x2, y2);
-            double l3 = PitagorTheoremDekartCoordinatSystem(x3, y3);
-            double l4 = PitagorTheoremDekartCoordinatSystem(x4, y4);
+            double l1 = Pitagor(x1, y1);
+            double l2 = Pitagor(x2, y2);
+            double l3 = Pitagor(x3, y3);
+            double l4 = Pitagor(x4, y4);
 
-            string result1 = string.Empty;
+            string result = string.Empty;
             string result2 = string.Empty;
-            double max1 = 0;
+
             double max2 = 0;
+            double[] arr = { x1, y1, x2, y2, x3, y3, x4, y4 };
+            double[] max = new double[2];
 
-            if (l1 > l2)
+            if (Pitagor(arr[0], arr[1]) > Pitagor(arr[2], arr[3]))
             {
-                max1 = l2;
-                result1 = $"({x2}, {y2})";
-            }
-            else
-            {
-                max1 = l1;
-                result1 = $"({x1}, {y1})";
+                max = new double { arr[0] arr[1] } ;
             }
 
-            if (l3 > l4)
-            {
-                max2 = l4;
-                result2 = $"({x4}, {y4})";
-            }
-            else
-            {
-                max2 = l3;
-                result2 = $"({x3}, {y3})";
-            }
 
-            if (max1 < max2)
-            {
-                Console.WriteLine(result1);
-                Console.WriteLine(result2);
-            }
-            else
-            {
-                Console.WriteLine(result2);
-                Console.WriteLine(result1);
-            }
+
+
+            Console.WriteLine(string.Join(" ", arr));
+            //if (Pitagor(x1, y1) > Pitagor(x2, y2))
+            //{
+            //    max = Pitagor(x1, y1);
+            //    result = $"({x1}, {y1})";
+            //}
+            //else
+            //{
+            //    max = Pitagor(x2, y2);
+            //    result = $"({x2}, {y2})";
+            //}
+
+
+            //if (Pitagor(x3, y3) > Pitagor(x4, y4))
+            //{
+            //    max2 = Pitagor(x3, y3);
+            //    result2 = $"({x3}, {y3})";
+            //}
+            //else
+            //{
+            //    max2 += Pitagor(x4, y4);
+            //    result2 = $"({x4}, {y4})";
+            //}
         }
-        static double PitagorTheoremDekartCoordinatSystem(double x1, double y1)
+
+        static void ReadInput(out double x1, out double y1, out double x2, out double y2, out double x3, out double y3, out double x4, out double y4)
+        {
+            x1 = double.Parse(Console.ReadLine());
+            y1 = double.Parse(Console.ReadLine());
+            x2 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
+            x3 = double.Parse(Console.ReadLine());
+            y3 = double.Parse(Console.ReadLine());
+            x4 = double.Parse(Console.ReadLine());
+            y4 = double.Parse(Console.ReadLine());
+        }
+
+        static double Pitagor(double x1, double y1)
         {
             return Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
         }
