f6cbaa556f3047d7e0fd777879f328e4621e757d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-13 10:43:58 +0300

    Add Lists

---
+++ Listslecture/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Listslecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = new List<int>();
+
+            number.Add(1);
+            number.Add(2);
+        }
+    }
+}
--- LongerLine/Program.cs
+++ LongerLine/Program.cs
@@ -21,39 +21,32 @@ namespace LongerLine
             double[] arr = { x1, y1, x2, y2, x3, y3, x4, y4 };
             double[] max = new double[2];
 
-            if (Pitagor(arr[0], arr[1]) > Pitagor(arr[2], arr[3]))
+            if (Pitagor(arr[0], arr[1]) >= Pitagor(arr[2], arr[3]))
             {
-                max = new double { arr[0] arr[1] } ;
+                max = new double[] { arr[0], arr[1] };
+            }
+            else
+            {
+                max = new double[] { arr[2], arr[3] };
+            }
+
+            if (Pitagor(max[0], max[1]) >= Pitagor(x3, y3))
+            { }
+            else
+            {
+                max = new double[] { x3, y3 };
             }
 
 
 
 
-            Console.WriteLine(string.Join(" ", arr));
-            //if (Pitagor(x1, y1) > Pitagor(x2, y2))
-            //{
-            //    max = Pitagor(x1, y1);
-            //    result = $"({x1}, {y1})";
-            //}
-            //else
-            //{
-            //    max = Pitagor(x2, y2);
-            //    result = $"({x2}, {y2})";
-            //}
 
 
-            //if (Pitagor(x3, y3) > Pitagor(x4, y4))
-            //{
-            //    max2 = Pitagor(x3, y3);
-            //    result2 = $"({x3}, {y3})";
-            //}
-            //else
-            //{
-            //    max2 += Pitagor(x4, y4);
-            //    result2 = $"({x4}, {y4})";
-            //}
         }
-
+        static double Pitagor(double x1, double y1)
+        {
+            return Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
+        }
         static void ReadInput(out double x1, out double y1, out double x2, out double y2, out double x3, out double y3, out double x4, out double y4)
         {
             x1 = double.Parse(Console.ReadLine());
@@ -64,11 +57,6 @@ namespace LongerLine
             y3 = double.Parse(Console.ReadLine());
             x4 = double.Parse(Console.ReadLine());
             y4 = double.Parse(Console.ReadLine());
-        }
-
-        static double Pitagor(double x1, double y1)
-        {
-            return Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
         }
     }
 }
