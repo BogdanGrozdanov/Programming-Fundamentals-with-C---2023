56f591fbad1f424dd71d3507cb42c00b7ec3e595
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-13 14:01:05 +0300

    Update Program.cs

--- Listslecture/Program.cs
+++ Listslecture/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 
 namespace Listslecture
 {
@@ -7,10 +8,16 @@ namespace Listslecture
         static void Main(string[] args)
         {
             List<int> number = new List<int>();
-
+            List<int> number2 = new List<int> { 1, 2, 3, 4 };
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
             number.Add(1);
             number.Add(2);
             number.Count();
+            List<int> numbers4 = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                numbers4.Add(int.Parse(Console.ReadLine()));
+            }
         }
     }
 }
