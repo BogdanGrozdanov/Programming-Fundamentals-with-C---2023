0f21b43779d647ad8411b5d8544d925bae6657d1
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 16:15:32 +0300

    Update Program.cs

--- TrainLists/Program.cs
+++ TrainLists/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int capacity = int.Parse(Console.ReadLine());
+            string[] comand = Console.ReadLine().Split();
+            while (comand[0] != "end")
+            {
+                if (comand[0].ToLower() == "add")
+                {
+                    wagons.Add(int.Parse(comand[1]));
+                }
+                else
+                {
+                    int numberOfPassenger = int.Parse(comand[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (numberOfPassenger + wagons[i] <= capacity)
+                        {
+                            wagons[i] = numberOfPassenger + wagons[i];
+                            break;
+                        }
+                    }
+                }
+                comand = Console.ReadLine().Split();
+            }
+            Console.WriteLine(string.Join(" ", wagons));
         }
     }
 }
