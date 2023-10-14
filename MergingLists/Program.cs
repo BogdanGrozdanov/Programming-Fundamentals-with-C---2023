27478ad6bee802017fdfee9e1cca163e95b2bcec
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 08:00:22 +0300

    Update Program.cs

--- MergingLists/Program.cs
+++ MergingLists/Program.cs
@@ -18,7 +18,9 @@ namespace MergingLists
                 Select(double.Parse)
                 .ToList();
             List<double> outputList = new List<double>();
+
             int endLooping = 0;
+
             if (firstNumbers.Count < secondNumbers.Count)
             { endLooping = firstNumbers.Count; }
             else { endLooping = secondNumbers.Count; }
@@ -28,7 +30,16 @@ namespace MergingLists
                 outputList.Add(firstNumbers[i]);
                 outputList.Add(secondNumbers[i]);
             }
-
+            if (firstNumbers.Count < secondNumbers.Count)
+            {
+                for (int i = endLooping; i < secondNumbers.Count; i++)
+                { outputList.Add(secondNumbers[i]); }
+            }
+            else
+            {
+                for (int i = endLooping; i < firstNumbers.Count; i++)
+                { outputList.Add(firstNumbers[i]); }
+            }
 
             Console.WriteLine(string.Join(" ", outputList));
         }
