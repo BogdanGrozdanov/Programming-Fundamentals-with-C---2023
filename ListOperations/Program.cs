b9d36b8962f54724df07fe31f7c52f5821e8c24d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 22:30:39 +0300

    Create Program.cs

---
+++ ListOperations/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intergers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] comand = Console.ReadLine().Split();
+            while (comand[0].ToLower() != "end")
+            {
+                if (comand[0] == "add")
+                {
+                    intergers.Add(int.Parse(comand[1]));
+                }
+                else if (comand[0].ToLower() == "insert")
+                {
+                    if (int.Parse(comand[2]) > intergers.Count - 1)
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    intergers.Insert(int.Parse(comand[2]), int.Parse(comand[1]));
+                }
+                else if (comand[0].ToLower() == "remove")
+                {
+                    if (int.Parse(comand[1]) > intergers.Count - 1)
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    else { intergers.RemoveAt(int.Parse(comand[1])); }
+
+                }
+                else if (comand[0].ToLower() == "shift" && comand[1].ToLower() == "left")
+                {
+                    for (int i = 0; i < int.Parse(comand[2]); i++)
+                    {
+                        int movedElement = intergers[0];
+                        intergers.RemoveAt(0);
+                        intergers.Add(movedElement);
+                    }
+                }
+                else if (comand[0].ToLower() == "shift" && comand[1].ToLower() == "right")
+                {
+                    int lastElement = intergers.Last();
+                    intergers.RemoveAt(intergers.Count - 1);
+                    intergers.Insert(0, lastElement);
+                }
+                comand = Console.ReadLine().Split();
+            }
+            Console.WriteLine(string.Join(" ", intergers));
+        }
+    }
+}
