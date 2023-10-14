80fdecb1e3ec83dd632474c3ad1bd4c6b7db5079
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 21:56:51 +0300

    Update Program.cs

--- HouseParty/Program.cs
+++ HouseParty/Program.cs
@@ -11,19 +11,20 @@ namespace HouseParty
             for (int i = 0; i < n; i++)
             {
                 string[] name = Console.ReadLine().Split(" ");
-                if (names.Contains(name[0]))
+
+                if (name[2] == "not")
+                {
+                    if (names.Contains(name[0])) { names.Remove(name[0]); }
+                    else { Console.WriteLine($"{name[0]} is not in the list!"); }
+                }
+                else if (names.Contains(name[0]))
                 {
                     Console.WriteLine($"{name[0]} is already in the list!");
                 }
-                else if (name[2] == "not")
-                {
-                    Console.WriteLine($"{name[0]} is not in the list!");
-
-                }
                 else { names.Add(name[0]); }
-               
             }
-            
+            foreach (string name in names) { Console.WriteLine(name); }
+
         }
     }
 }
