f5dbe6e2a7f3843a92a6497e686c2ca95068a67f
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 10:17:34 +0300

    Manipulation array

---
+++ ListManipulationAdvanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListManipulationAdvanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- ListManipulationBasics/Program.cs
+++ ListManipulationBasics/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<double> numbers =
+                 Console.ReadLine()
+                 .Split().
+                 Select(double.Parse)
+                 .ToList();
+
+            string[] comand = Console.ReadLine().Split();
+            while (comand[0] != "end")
+            {
+                if (comand[0] == "Add")
+                {
+                    numbers.Add(int.Parse(comand[1]));
+                }
+                else if (comand[0] == "Remove")
+                {
+                    numbers.Remove(int.Parse(comand[1]));
+                }
+                else if (comand[0] == "RemoveAt")
+                { numbers.RemoveAt(int.Parse(comand[1])); }
+                else if (comand[0] == "Insert")
+                { numbers.Insert(int.Parse(comand[2]), int.Parse(comand[1])); }
+
+                comand = Console.ReadLine().Split();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
