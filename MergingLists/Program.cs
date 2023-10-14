c21e262447c66169ffe9fae9c978cafe01b70532
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 07:52:52 +0300

    Update Program.cs

--- MergingLists/Program.cs
+++ MergingLists/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace MergingLists
+﻿using System.ComponentModel.Design;
+using System.Globalization;
+
+namespace MergingLists
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<double> firstNumbers =
+                Console.ReadLine()
+                .Split().
+                Select(double.Parse)
+                .ToList();
+            List<double> secondNumbers =
+                Console.ReadLine()
+                .Split().
+                Select(double.Parse)
+                .ToList();
+            List<double> outputList = new List<double>();
+            int endLooping = 0;
+            if (firstNumbers.Count < secondNumbers.Count)
+            { endLooping = firstNumbers.Count; }
+            else { endLooping = secondNumbers.Count; }
+
+            for (int i = 0; i < endLooping; i++)
+            {
+                outputList.Add(firstNumbers[i]);
+                outputList.Add(secondNumbers[i]);
+            }
+
+            Console.WriteLine(string.Join(" ", outputList));
         }
     }
 }
