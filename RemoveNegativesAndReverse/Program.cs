25ee9dc506e1c6699a8cfdfcc144d352ca5a02ca
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 08:24:57 +0300

    Create Program.cs

---
+++ RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers =
+                 Console.ReadLine()
+                 .Split().
+                 Select(double.Parse)
+                 .ToList();
+            List<double> outputNumbers = new List<double>();
+            foreach (double number in numbers) { if (number >= 0) { outputNumbers.Add(number); } }
+            //for (int i = numbers.Count - 1; i > 0; i--)
+            //{
+            //    if (numbers[i] >= 0) { Console.Write(numbers[i] + " "); }
+            //}
+            outputNumbers.Reverse();
+            if (outputNumbers.Count == 0) { Console.WriteLine("empty"); }
+            else { Console.WriteLine(string.Join(" ", outputNumbers)); }
+
+        }
+    }
+}
