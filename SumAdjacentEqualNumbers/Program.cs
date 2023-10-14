74be3af81a6f2cac5c0050e2fa997fe12d63b924
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 06:59:09 +0300

    More ex.

---
+++ SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SumAdjacentEqualNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+            SumAdjacentEqualNumbers(numbers);
+            Console.WriteLine(string.Join(" ", numbers.ToArray()));
+        }
+
+        static void SumAdjacentEqualNumbers(List<double> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    double curentSum = numbers[i] + numbers[i + 1];
+                    numbers.Remove(numbers[i]);
+                    numbers.Remove(numbers[i]);
+                    numbers.Insert(i, curentSum);
+                    i = -1;
+                }
+            }
+        }
+    }
+}
