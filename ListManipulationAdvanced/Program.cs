2ace4f08419bacb844ce293aeed15924779464b0
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 13:21:19 +0300

    Update Program.cs

--- ListManipulationAdvanced/Program.cs
+++ ListManipulationAdvanced/Program.cs
@@ -9,8 +9,14 @@
                  .Split().
                  Select(double.Parse)
                  .ToList();
+            ListManipulationAdvans(numbers);
 
+        }
+
+        static void ListManipulationAdvans(List<double> numbers)
+        {
             string[] comand = Console.ReadLine().Split();
+            int couter = 0;
             while (comand[0].ToLower() != "end")
             {
                 if (comand[0].ToLower() == "contains")
@@ -77,8 +83,32 @@
                         Console.WriteLine();
                     }
                 }
+
+                if (comand[0].ToLower() == "add")
+                {
+                    numbers.Add(int.Parse(comand[1]));
+                    couter++;
+                }
+                else if (comand[0].ToLower() == "remove")
+                {
+                    numbers.Remove(int.Parse(comand[1]));
+                    couter++;
+                }
+                else if (comand[0].ToLower() == "removeat")
+                {
+                    numbers.RemoveAt(int.Parse(comand[1]));
+                    couter++;
+                }
+                else if (comand[0].ToLower() == "insert")
+                {
+                    numbers.Insert(int.Parse(comand[2]), int.Parse(comand[1]));
+                    couter++;
+                }
                 comand = Console.ReadLine().Split();
             }
+            if (couter > 0) { Console.WriteLine(string.Join(" ", numbers)); }
         }
+
+
     }
 }
