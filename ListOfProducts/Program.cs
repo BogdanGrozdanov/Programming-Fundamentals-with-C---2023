db662da2057366bdf71b851d6b8c2938259966c3
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 08:08:18 +0300

    Create Program.cs

---
+++ ListOfProducts/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ListOfProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> productList = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                productList.Add(input);
+            }
+            productList.Sort();
+            for (int i = 0;i < productList.Count;i++) 
+            { Console.WriteLine($"{i+1}.{productList[i]}"); }
+        }
+    }
+}
