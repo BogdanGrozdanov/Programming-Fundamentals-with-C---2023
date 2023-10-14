690cf34b45f09f03b956c0d361d797e0e7003d4a
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 08:27:05 +0300

    Create Program.cs

---
+++ ListManipulationBasics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListManipulationBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
