845e2626dc2ebeba2499b0047ff30780932833de
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 16:32:42 +0300

    Update Program.cs

--- ChangeList/Program.cs
+++ ChangeList/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] comand = Console.ReadLine().Split();
+            while (comand[0].ToLower() != "end")
+            {
+                if (comand[0].ToLower() == "delete")
+                {
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (int.Parse(comand[1]) == numbers[i]) { numbers.Remove(int.Parse(comand[1])); }
+                    }
+                }
+                else if (comand[0].ToLower() == "insert")
+                {
+                    int index = int.Parse(comand[2]);
+                    int element = int.Parse(comand[1]);
+                    numbers.Insert(index, element);
+                }
+                comand = Console.ReadLine().Split();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
