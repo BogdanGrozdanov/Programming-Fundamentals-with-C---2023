f416917aa1a9035eab74f2a53f59f53fd3238dc8
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 18:58:55 +0300

    Update Program.cs

--- HouseParty/Program.cs
+++ HouseParty/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace HouseParty
+﻿using System.Collections.Generic;
+
+namespace HouseParty
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] name = Console.ReadLine().Split(" ");
+                if (names.Contains(name[0]))
+                {
+                    Console.WriteLine($"{name[0]} is already in the list!");
+                }
+                else if (name[2] == "")
+                {
+                    names.Add(name[0]);
+                }
+            }
+            for (int i = 0; i < names.Count; i++)
+            {
+
+            }
         }
     }
 }
