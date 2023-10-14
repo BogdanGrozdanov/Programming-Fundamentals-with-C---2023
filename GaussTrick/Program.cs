50c5f542232dadd6552dcad285fe60db77e79a45
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-14 07:25:18 +0300

    Update Program.cs

--- GaussTrick/Program.cs
+++ GaussTrick/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<double> numbers =
+                Console.ReadLine()
+                .Split().
+                Select(double.Parse)
+                .ToList();
+
+            List<double> sumNumbers = new List<double>();
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                double left = numbers[i];
+                double right = numbers[numbers.Count - 1 - i];
+                sumNumbers.Add(left + right);
+            }
+            if (numbers.Count % 2 == 1)
+            {
+                sumNumbers.Add(numbers[numbers.Count / 2]);
+            }
+            Console.WriteLine(string.Join(" ", sumNumbers));
+
         }
     }
 }
