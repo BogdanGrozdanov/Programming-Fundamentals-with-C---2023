6713efaccffd687f492ab9b56b5ab9c3185f5043
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-15 20:47:47 +0300

    Update Program.cs

--- BombNumbers/Program.cs
+++ BombNumbers/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> bombsNumber = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> bombAndPower = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int bomb = bombAndPower[0];
+            int power = bombAndPower[1];
+
+            for (int i = 0; i < bombsNumber.Count; i++)
+            {
+               
+               
+            }
         }
     }
 }
