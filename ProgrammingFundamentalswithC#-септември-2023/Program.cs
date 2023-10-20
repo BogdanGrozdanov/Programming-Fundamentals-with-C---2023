103cf6ae0927584fdec1703fdd738f7950e976a0
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-20 17:48:45 +0300

    Update Program.cs

--- ProgrammingFundamentalswithC#-септември-2023/Program.cs
+++ ProgrammingFundamentalswithC#-септември-2023/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace ProgrammingFundamentalswithC__септември_2023
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace ProgrammingFundamentalswithC__септември_2023
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( 125%10 );
+            List<int> sequenceOftarget = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //0 1 2 3 4 5 6 7 8 9
+            sequenceOftarget.RemoveRange(5,2);
+            Console.WriteLine(string.Join(" ", sequenceOftarget));
         }
     }
 }
