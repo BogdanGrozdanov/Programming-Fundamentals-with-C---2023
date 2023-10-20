ef5bee0750b52ba2b26d6a077d1a2d695ad371c2
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-20 13:19:41 +0300

    Update Program.cs

--- TheLift/Program.cs
+++ TheLift/Program.cs
@@ -7,19 +7,30 @@
             int people = int.Parse(Console.ReadLine());
             int[] numWagons = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+            int capacityWagons = 4;
+
             for (int i = 0; i < numWagons.Length; i++)
             {
                 if (numWagons[i] < 4)
                 {
-                    
-                    numWagons[i] += people;
-                    people--;
+                    int peopleAdd = capacityWagons - numWagons[i];
+                    if (people < capacityWagons)
+                    {
+                        peopleAdd = people;
+                    }
+                    numWagons[i] += peopleAdd;
+                    people -= numWagons[i];
                 }
             }
-            if (numWagons.Contains(3))
+
+            for (int i = 0; i < numWagons.Length; i++)
             {
-                Console.WriteLine($"The lift has empty spots!{string.Join(" ", numWagons)}");
+                if (numWagons[i] < capacityWagons)
+                {
+                    Console.WriteLine($"The lift has empty spots!{string.Join(" ", numWagons)}");
+                }
             }
+
             if (people > 0)
             {
                 Console.WriteLine($"There isn't enough space! {people} people in a queue!{string.Join(" ", numWagons)}");
