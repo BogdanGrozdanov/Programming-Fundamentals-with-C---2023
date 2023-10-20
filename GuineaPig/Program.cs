5fa87f2b8762553b469eee4c1078c5f16d5ffaaa
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-20 12:08:18 +0300

    Update Program.cs

--- GuineaPig/Program.cs
+++ GuineaPig/Program.cs
@@ -12,22 +12,28 @@
             double foodPerMount = 0;
             double hayPerMount = 0;
             double coverPerMount = 0;
-            while (foodPerMount >= foodQuantity && hayQuantiy <= hayPerMount && coverQuantity <= coverPerMount)
+
+            for (int i = 1; i <= 30; i++)
             {
-                for (int i = 1; i <= 30; i++)
+
+                foodPerMount += 0.300;
+                if (i % 2 == 0)
                 {
-                    foodPerMount += 0.3;
-                    if (i % 2 == 0)
-                    {
-                        hayPerMount = (foodQuantity - foodPerMount) - 0.05;
-                    }
-                    if (i % 3 == 0)
-                    {
-                        coverPerMount = guineaWeight / 3.0;
-                    }
+                    hayPerMount += (foodQuantity - foodPerMount) * 0.05;
                 }
+                coverPerMount = (guineaWeight / 3) * 10;
+
+
+
             }
-            Console.WriteLine($"Everything is fine! Puppy is happy! Food: {foodQuantity - foodPerMount:F2}, Hay: {hayQuantiy - hayPerMount:F2}, Cover: {coverQuantity - coverPerMount:2}.");
+            if (foodQuantity <= foodPerMount || hayQuantiy <= hayPerMount || coverQuantity <= coverPerMount)
+            { Console.WriteLine("Merry must go to the pet store!"); }
+            else
+            {
+                Console.WriteLine($"Everything is fine! Puppy is happy! Food: {(foodQuantity - foodPerMount):F2}, Hay: {(hayQuantiy - hayPerMount):F2}, Cover: {(coverQuantity - coverPerMount):F2}.");
+            }
+
+
 
 
         }
