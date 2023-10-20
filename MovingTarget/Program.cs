c94105c1f5b415b49c1155ea8b46125a87a7da22
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-20 20:38:16 +0300

    Update Program.cs

--- MovingTarget/Program.cs
+++ MovingTarget/Program.cs
@@ -1,18 +1,27 @@
 ﻿namespace MovingTarget
 {
     internal class Program
-    {
+    {/*
+      52 74 23 44 96 110
+      Shoot 5 10
+      Shoot 1 80
+      Strike 2 1
+      Add 22 3
+      End
+      
+      */
         static void Main(string[] args)
         {
             List<int> sequenceOftarget = Console.ReadLine().Split().Select(int.Parse).ToList();
             List<string> comand = Console.ReadLine().Split().ToList();
 
-            string action = comand[0];
-            int index = int.Parse(comand[1]);
-            int value = int.Parse(comand[2]);
+            
 
-            while (action != "end")
+            while (comand[0] != "End")
             {
+                string action = comand[0];
+                int index = int.Parse(comand[1]);
+                int value = int.Parse(comand[2]);
                 if (action == "Shoot")
                 {
                     if (index < sequenceOftarget.Count)
@@ -35,14 +44,15 @@
                 }
                 else if (action == "Strike")
                 {
-                    if ((index - value) >= 0 && (index + value) <= sequenceOftarget.Count - 1)
+                    if ((index - value) >= 0 && (index + value) < sequenceOftarget.Count - 1)
                     {
-                        sequenceOftarget.RemoveRange(index, value);
-                        sequenceOftarget.RemoveAt(index + 1,);
+                        sequenceOftarget.RemoveRange(index - value, (value * 2)+1 );
                     }
-
+                    else { Console.WriteLine("Strike missed!"); }
                 }
+                comand = Console.ReadLine().Split().ToList();
             }
+            Console.WriteLine(string.Join("|", sequenceOftarget));
         }
     }
 }
