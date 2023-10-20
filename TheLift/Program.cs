d41818f3fa144deaf8445cc5a8897e76ff02cfd2
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-20 12:26:09 +0300

    Update Program.cs

--- TheLift/Program.cs
+++ TheLift/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int people = int.Parse(Console.ReadLine());
+            int[] numWagons = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numWagons.Length; i++)
+            {
+                if (numWagons[i] < 4)
+                {
+                    
+                    numWagons[i] += people;
+                    people--;
+                }
+            }
+            if (numWagons.Contains(3))
+            {
+                Console.WriteLine($"The lift has empty spots!{string.Join(" ", numWagons)}");
+            }
+            if (people > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {people} people in a queue!{string.Join(" ", numWagons)}");
+
+            }
         }
     }
 }
