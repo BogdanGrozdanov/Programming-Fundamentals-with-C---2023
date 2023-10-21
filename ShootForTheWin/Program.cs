1456561e4c28ddeba3600dc12fbd312f83558828
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-21 21:27:59 +0300

    Create Program.cs

---
+++ ShootForTheWin/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ShootForTheWin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string comand = Console.ReadLine();
+            int counter = 0;
+            while (comand != "End")
+            {
+                int index = int.Parse(comand);
+
+                if (targets.Count - 1 >= index)
+                {
+
+                    var curentElement = targets[index];
+
+                    targets[index] = -1;
+                    for (int i = 0; i < targets.Count; i++)
+                    {
+                        if (targets[i] >= 0 && targets[i] > curentElement) { targets[i] -= curentElement; counter++; }
+                        else if (targets[i] >= 0 && targets[i] <= curentElement) { targets[i] += curentElement; counter++; }
+                    }
+                }
+                comand = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", targets));
+        }
+    }
+}
