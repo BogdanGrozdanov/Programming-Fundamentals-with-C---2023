2201fa8974a55a5e00b409e961ee88bb376579ad
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-21 19:41:54 +0300

    Update Program.cs

--- CounterStrike/Program.cs
+++ CounterStrike/Program.cs
@@ -4,12 +4,30 @@
     {
         static void Main(string[] args)
         {
-            int initialEnergy =int.Parse(Console.ReadLine());
-            string comand =Console.ReadLine();
-            while (initialEnergy > 0) 
+            int initialEnergy = int.Parse(Console.ReadLine());
+            string[] comand = Console.ReadLine().Split();
+            bool flag = false;
+            int counter = 0;
+
+            while (!comand.Contains("End"))
             {
-            
+                int distance = int.Parse(comand[0]);
+                if (distance <= initialEnergy)
+                {
+                    counter++;
+                    initialEnergy -= distance;
+                    if (counter % 3 == 0) { initialEnergy += counter; }
+                }
+                else
+                {
+                    flag = true;
+                    Console.WriteLine($"Not enough energy! Game ends with {counter} won battles and {initialEnergy} energy.");
+                    break;
+                }
+                comand = Console.ReadLine().Split();
             }
+
+            if (!flag) { Console.WriteLine($"Won battles: {counter}. Energy left: {initialEnergy}"); }
         }
     }
 }
