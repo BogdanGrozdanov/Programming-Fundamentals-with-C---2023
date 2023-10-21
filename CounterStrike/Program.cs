062a6e307e6d5a11b44bd4257989ea0ca1de362f
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-21 09:28:01 +0300

    Create Program.cs

---
+++ CounterStrike/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CounterStrike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialEnergy =int.Parse(Console.ReadLine());
+            string comand =Console.ReadLine();
+            while (initialEnergy > 0) 
+            {
+            
+            }
+        }
+    }
+}
