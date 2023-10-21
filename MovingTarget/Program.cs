04e19e349b2886733e14a68c3520f067b0631741
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-21 09:19:27 +0300

    Update Program.cs

--- MovingTarget/Program.cs
+++ MovingTarget/Program.cs
@@ -1,28 +1,22 @@
 ﻿namespace MovingTarget
 {
     internal class Program
-    {/*
-      52 74 23 44 96 110
-      Shoot 5 10
-      Shoot 1 80
-      Strike 2 1
-      Add 22 3
-      End
-      
-      */
+    {
         static void Main(string[] args)
         {
             List<int> sequenceOftarget = Console.ReadLine().Split().Select(int.Parse).ToList();
             List<string> comand = Console.ReadLine().Split().ToList();
+            comand = MoveTargetLogic(sequenceOftarget, comand);
+        }
 
-            
-
-            while (comand[0] != "End")
+        static List<string> MoveTargetLogic(List<int> sequenceOftarget, List<string> comand)
+        {
+            while (comand[0].ToLower() != "end")
             {
                 string action = comand[0];
                 int index = int.Parse(comand[1]);
                 int value = int.Parse(comand[2]);
-                if (action == "Shoot")
+                if (action.ToLower() == "shoot")
                 {
                     if (index < sequenceOftarget.Count)
                     {
@@ -31,10 +25,9 @@
                         {
                             sequenceOftarget.RemoveAt(index);
                         }
-
                     }
                 }
-                else if (action == "Add")
+                else if (action.ToLower() == "add")
                 {
                     if (index < sequenceOftarget.Count)
                     {
@@ -42,17 +35,18 @@
                     }
                     else { Console.WriteLine("Invalid placement!"); }
                 }
-                else if (action == "Strike")
+                else if (action.ToLower() == "strike")
                 {
                     if ((index - value) >= 0 && (index + value) < sequenceOftarget.Count - 1)
                     {
-                        sequenceOftarget.RemoveRange(index - value, (value * 2)+1 );
+                        sequenceOftarget.RemoveRange(index - value, (value * 2) + 1);
                     }
                     else { Console.WriteLine("Strike missed!"); }
                 }
                 comand = Console.ReadLine().Split().ToList();
             }
             Console.WriteLine(string.Join("|", sequenceOftarget));
+            return comand;
         }
     }
 }
