a500119a72dacabb614612229ba22ca7bf7c3318
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-22 10:35:59 +0300

    Update Program.cs

--- FriendListMaintenance/Program.cs
+++ FriendListMaintenance/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> names = Console.ReadLine().Split(", ").ToList();
+            string[] comand = Console.ReadLine().Split();
+
+            List<string> blackListName = new List<string>();
+            while (comand[0] != "Report")
+            {
+                if (comand[0] == "Blacklist")
+                {
+                    if (!blackListName.Contains(comand[1]))
+                    { Console.WriteLine($"{comand[1]} was not found."); }
+                    else
+                    {
+                        blackListName.Add(comand[1]);
+                        Console.WriteLine($"{comand[1]} was blacklisted.");
+                    }
+                }
+                else if (comand[0] == "Error")
+                {
+
+                }
+                else if (comand[0] == "Change")
+                {
+                    if (int.Parse(comand[1]) < names.Count)
+                    {
+                        string curentName = names[int.Parse(comand[1])];
+                        names[int.Parse(comand[1])] = comand[2];
+
+                    }
+                }
+            }
         }
     }
 }
