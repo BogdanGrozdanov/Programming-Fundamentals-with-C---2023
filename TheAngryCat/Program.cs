b26ccc05503791d17ccd51725511996cfdab38b6
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-22 11:43:27 +0300

    Create Program.cs

---
+++ TheAngryCat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TheAngryCat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
