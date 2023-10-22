e7255d135d419a35a7f61bcee70d78a57088e74e
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-22 10:12:44 +0300

    Create Program.cs

---
+++ FriendListMaintenance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FriendListMaintenance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
