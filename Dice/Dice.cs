d85338b29b906c81ec22fc6d80518be666b05e3e
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-25 12:45:18 +0300

    Dice class

---
+++ Dice/Dice.cs
@@ -0,0 +1,12 @@
+﻿namespace Dice
+{
+    internal class Dice
+    {
+        public int Sides { get; set; }
+        public int Roll()
+        {
+            Random random = new Random();
+            return random.Next(1, Sides + 1);
+        }
+    }
+}
---
+++ Dice/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Dice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            dice.Sides = 6;
+            int result = dice.Roll();
+            Console.WriteLine(result);
+        }
+    }
+}
