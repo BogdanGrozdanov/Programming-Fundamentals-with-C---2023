d59c5053c0db46a2e4db1735a86a3721e1642c7f
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-27 12:54:40 +0300

    Create Program.cs

---
+++ Students2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Students2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
