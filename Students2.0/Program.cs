5a8bdc65e1df287bcce4a68f80d5f260de2225df
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-27 13:16:27 +0300

    Update Program.cs

--- Students2.0/Program.cs
+++ Students2.0/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Student> students = new List<Student>();
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            while (input[0] != "end")
+            {
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                string town = input[3];
+
+                Student student = new Student();
+                Student ExsitStudent = new Student();
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Age = age;
+                student.HomeTown = town;
+                foreach (var item in students)
+                {
+                    if (item.FirstName == student.FirstName && item.LastName == student.LastName)
+                    {
+                        student = student;
+                        students.Add(student);
+                    }
+                    else
+                    {
+                        students.Add(student);
+                    }
+                }
+
+
+                input = Console.ReadLine().Split().ToArray();
+            }
+            string filterByTown = Console.ReadLine();
+            List<Student> sortByTown = students.Where(s => s.HomeTown == filterByTown).ToList();
+
+            foreach (Student student in sortByTown)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
         }
+
+    }
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
     }
 }
