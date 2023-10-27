2e97a11b84c7c1be6eb6df8b4b7de7adcbd2ab2d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-27 18:04:23 +0300

    Update Program.cs

--- Students/Program.cs
+++ Students/Program.cs
@@ -15,17 +15,39 @@ namespace Students
                 string lastName = input[1];
                 int age = int.Parse(input[2]);
                 string town = input[3];
+                AddNotExistStudent(students, firstName, lastName, age, town);
 
-                Student student = new Student();
+                input = Console.ReadLine().Split().ToArray();
+            }
+            string filterByTown = Console.ReadLine();
+            SortByTown(students, filterByTown);
+        }
 
+        public static void AddNotExistStudent(List<Student> students, string firstName, string lastName, int age, string town)
+        {
+            Student student = students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+            if (student == null)
+            {
+                students.Add(new Student()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age,
+                    HomeTown = town
+                });
+            }
+            else
+            {
                 student.FirstName = firstName;
                 student.LastName = lastName;
                 student.Age = age;
                 student.HomeTown = town;
-                students.Add(student);
-                input = Console.ReadLine().Split().ToArray();
+
             }
-            string filterByTown = Console.ReadLine();
+        }
+
+        public static void SortByTown(List<Student> students, string filterByTown)
+        {
             List<Student> sortByTown = students.Where(s => s.HomeTown == filterByTown).ToList();
             foreach (Student student in sortByTown)
             {
