60783394042c65bcb72f4ae4e0dbbab67fc0462d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-27 12:53:48 +0300

    Update Program.cs

--- Students/Program.cs
+++ Students/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Students
+﻿using System.Security.Cryptography;
+
+namespace Students
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Student> students = new List<Student>();
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            while (input[0] != "end")
+            {
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                string town = input[3];
+
+                Student student = new Student();
+
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Age = age;
+                student.HomeTown = town;
+                students.Add(student);
+                input = Console.ReadLine().Split().ToArray();
+            }
+            string filterByTown = Console.ReadLine();
+            List<Student> sortByTown = students.Where(s => s.HomeTown == filterByTown).ToList();
+            foreach (Student student in sortByTown)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
         }
     }
     public class Student
