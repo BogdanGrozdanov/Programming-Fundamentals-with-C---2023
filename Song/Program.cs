ea81f8f24e74dec4418ec0ed67b2d92c309c39b2
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-27 10:12:31 +0300

    add Song

---
+++ Song/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Song
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numSong = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+            for (int i = 0; i < numSong; i++)
+            {
+                string[] data = Console.ReadLine().Split("_");
+                string type = data[0];
+                string name = data[1];
+                string time = data[2];
+
+                Song song = new Song();
+                song.TypeList = type;
+                song.Name = name;
+                song.Time = time;
+                songs.Add(song);
+
+            }
+        }
+    }
+}
