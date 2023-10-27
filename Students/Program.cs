727d801aaf4afaa35b71164d9208f68471c28899
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-27 11:40:23 +0300

    Update Program.cs

--- Students/Program.cs
+++ Students/Program.cs
@@ -7,4 +7,11 @@
             Console.WriteLine("Hello, World!");
         }
     }
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+    }
 }
