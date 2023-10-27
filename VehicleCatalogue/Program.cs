0d9721e37c7b0fca726aa60d320619bc0d7d87fe
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-27 23:26:38 +0300

    Update Program.cs

--- VehicleCatalogue/Program.cs
+++ VehicleCatalogue/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] input = Console.ReadLine().Split("/");
+            Catalog catalogs = new Catalog();
+            while (input[0] != "end")
+            {
+                string type = input[0];
+                string brand = input[1];
+                string model = input[2];
+                int weightOrHp = int.Parse(input[3]);
+                if (type == "Truck")
+                {
+                    Truck truck = new Truck();
+                    truck.Brand = brand;
+                    truck.Model = model;
+                    truck.Weight = weightOrHp;
+                    catalogs.Trucks.Add(truck);
+                }else if (type =="Car")
+                {
+                    Car car = new Car();
+                    car.Brand = brand;
+                    car.Model = model;
+                    car.HorsePower = weightOrHp;
+                    catalogs.Cars.Add(car);
+                }
+            }
+
+
+
         }
     }
     internal class Truck
@@ -23,7 +49,7 @@
     {
         public Catalog()
         {
-            Cars = new List<Car>();
+            this.Cars = new List<Car>();
             Trucks = new List<Truck>();
         }
         public List<Car> Cars { get; set; }
