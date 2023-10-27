e8ae6fe6f54952730d347c56b181774a4f2f8aa9
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-27 18:08:55 +0300

    Create Program.cs

---
+++ StoreBoxes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreBoxes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
