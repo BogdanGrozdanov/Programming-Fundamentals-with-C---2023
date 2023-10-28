ba80a0e262bdb7fa9786b0879ac740c0f8bfa75d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-10-28 12:03:11 +0300

    Update Program.cs

--- AdvertisementMessage/Program.cs
+++ AdvertisementMessage/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." };
+
+            string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+
+            string[] autors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
         }
     }
 }
