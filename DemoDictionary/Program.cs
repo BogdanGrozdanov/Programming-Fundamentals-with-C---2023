5d33a274c48c1a16d3ed73d3ddf379772db18911
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-01 16:59:15 +0200

    Create Program.cs

---
+++ DemoDictionary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
