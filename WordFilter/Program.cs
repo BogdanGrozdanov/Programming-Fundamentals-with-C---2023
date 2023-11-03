a026e4476c13b2c232fb047d590b0be730907795
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-03 12:41:31 +0200

    add more exercise

---
+++ WordFilter/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(s => s.Length % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine,strings));
+        }
+    }
+}
