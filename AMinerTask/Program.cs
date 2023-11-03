92f7bdfbeca69d1446ba6f2da93725c2d901b7bb
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-03 20:26:34 +0200

    More :}

---
+++ AMinerTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- CountCharsInAString/Program.cs
+++ CountCharsInAString/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] inputString = InputStringsArray();
+
+            Dictionary<char, int> strings = new Dictionary<char, int>();
+            InputAndCountDiferentChars(inputString, strings);
+            PrintOutputCountCharsInStrings(strings);
+        }
+
+        public static void PrintOutputCountCharsInStrings(Dictionary<char, int> strings)
+        {
+            foreach (var item in strings)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+
+        public static void InputAndCountDiferentChars(string[] inputString, Dictionary<char, int> strings)
+        {
+            foreach (string input in inputString)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (!strings.ContainsKey(input[i]))
+                    {
+                        strings.Add(input[i], 1);
+                    }
+                    else
+                    {
+                        strings[input[i]]++;
+                    }
+                }
+            }
+        }
+
+        public static string[] InputStringsArray()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
         }
     }
 }
