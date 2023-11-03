3f89c5e617d8613ae23fe5023deae8a29143fea0
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-03 12:18:04 +0200

    Dictionary exercise

---
+++ AssociativeArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AssociativeArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+
+            foreach (double d in numbers)
+            {
+                if (!count.ContainsKey(d))
+                {
+                    count.Add(d, 1);
+                }
+                else
+                {
+                    count[d]++;
+                }
+            }
+
+            foreach (var number in count)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
---
+++ OddOccurrences/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .ToLower()
+                .Split()
+                .GroupBy(x => x)
+                .Where(x => x.Count() % 2 == 1)
+                .SelectMany(x => x)
+                .Distinct()
+                .ToList();
+
+            Console.WriteLine(string.Join(" ",input));
+        }
+    }
+}
