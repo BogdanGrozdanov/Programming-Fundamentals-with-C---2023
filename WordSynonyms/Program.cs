ad34f7fd0bf104a88e72b3be18d22ebd9a115a61
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-03 12:32:46 +0200

    Update Program.cs

--- WordSynonyms/Program.cs
+++ WordSynonyms/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace WordSynonyms
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WordSynonyms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int wordCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> wordSinonym = new Dictionary<string, List<string>>();
+            for (int i = 0; i < wordCount; i++)
+            {
+                string word = Console.ReadLine();
+                string sinonym = Console.ReadLine();
+
+                if (!wordSinonym.ContainsKey(word))
+                {
+                    wordSinonym.Add(word, new List<string>());
+                    wordSinonym[word].Add(sinonym);
+                }
+                else { wordSinonym[word].Add(sinonym); }
+            }
+            foreach (var item in wordSinonym)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
         }
     }
 }
