e850ef0f1629c332cb07798811533d64caab973c
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-03 21:00:12 +0200

    Update Program.cs

--- OrdersAssociative/Program.cs
+++ OrdersAssociative/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, Orders> kvp = new Dictionary<string, Orders>();
+            while (true)
+            {
+                string[] orders = InputStringsArray();
+                if (orders[0] == "buy") { break; }
+                string item = orders[0];
+                double price = double.Parse(orders[1]);
+                double quantity = double.Parse(orders[2]);
+            }
+
         }
+        public static string[] InputStringsArray()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+        }
+    }
+    public class Orders
+    {
+        public Orders(double price, double quantity)
+        {
+            Price = price;
+            Quantity = quantity;
+        }
+        public double Price { get; set; }
+        public double Quantity { get; set; }
     }
 }
