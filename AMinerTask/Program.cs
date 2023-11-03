553e7a9561b6bf1d87ac580f2df2c527a0cb410d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-03 20:43:10 +0200

    Update Program.cs

--- AMinerTask/Program.cs
+++ AMinerTask/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace AMinerTask
+﻿using Microsoft.VisualBasic;
+
+namespace AMinerTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, int> allResourse = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if (resource == "stop") { break; }
+                int quantity = int.Parse(Console.ReadLine());
+                if (!allResourse.ContainsKey(resource))
+                {
+                    allResourse.Add(resource, quantity);
+                }
+                else
+                {
+                    allResourse[resource] += quantity;
+                }
+            }
+            PrintOutputResoursesAndQuantityOfResourses(allResourse);
+
+
+        }
+        public static void PrintOutputResoursesAndQuantityOfResourses
+                           (Dictionary<string, int> strings)
+        {
+            foreach (var item in strings)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
         }
     }
 }
