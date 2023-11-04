dc5b07ab5610a84567d031bab97d6fb15391a339
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-04 13:16:55 +0200

    Update Program.cs

--- OrdersAssociative/Program.cs
+++ OrdersAssociative/Program.cs
@@ -4,33 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Dictionary<string, double> kvp = new Dictionary<string, double>();
+            Dictionary<string, Orders> kvp = new Dictionary<string, Orders>();
 
             while (true)
             {
                 string[] orders = InputStringsArray();
+
                 if (orders[0] == "buy") { break; }
+
                 string item = orders[0];
                 double price = double.Parse(orders[1]);
                 double quantity = double.Parse(orders[2]);
+
+                Orders order = new Orders(price, quantity);
+
                 if (!kvp.ContainsKey(item))
                 {
-                    kvp.Add(item, price * quantity);
+                    kvp.Add(item, order);
                 }
                 else
                 {
-                    kvp[item] += price * quantity;
+                    kvp[item].Price = price;
+                    kvp[item].Quntity += quantity;
                 }
             }
             PrintOutput(kvp);
-
         }
-        public static void PrintOutput
-                           (Dictionary<string, double> strings)
+        public static void PrintOutput(Dictionary<string, Orders> strings)
         {
             foreach (var item in strings)
             {
-                Console.WriteLine($"{item.Key} -> {item.Value:F2}");
+                Console.WriteLine($"{item.Key} -> {item.Value.TotalPrice():F2}");
             }
         }
         public static string[] InputStringsArray()
@@ -38,6 +42,21 @@
             return Console.ReadLine()
                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .ToArray();
+        }
+    }
+    public class Orders
+    {
+        public Orders(double price, double quantity)
+        {
+            Price = price;
+            Quntity = quantity;
+        }
+        public double Price { get; set; }
+        public double Quntity { get; set; }
+
+        public double TotalPrice()
+        {
+            return Price * Quntity;
         }
     }
 }
