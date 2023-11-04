03afebf6840a03eb6b65bc044469a94c00f8768b
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-04 14:46:23 +0200

    Update Program.cs

--- SoftUniParking/Program.cs
+++ SoftUniParking/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfComands = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+
+            for (int i = 0; i < numberOfComands; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(" ");
+
+                string comand = inputs[0];
+                string owner = inputs[1];
+                if (comand == "register")
+                {
+                    string licensePlateNumber = inputs[2];
+
+                    if (!keyValuePairs.ContainsKey(owner))
+                    {
+                        keyValuePairs.Add(owner, licensePlateNumber);
+                        Console.WriteLine($"{owner} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                }
+                else if (comand == "unregister")
+                {
+                    if (!keyValuePairs.ContainsKey(owner))
+                    {
+                        Console.WriteLine($"ERROR: user {owner} not found");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{owner} unregistered successfully");
+                        keyValuePairs.Remove(owner);
+                    }
+                }
+            }
+            foreach (var user in keyValuePairs)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
         }
     }
 }
