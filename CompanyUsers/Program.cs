567e80c0a42e5a26076721dbb8b27a7d505fd80d
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-04 19:51:25 +0200

    Update Program.cs

--- CompanyUsers/Program.cs
+++ CompanyUsers/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, List<string>> kvpCompany = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" -> ");
+
+                if (input[0] == "End") { break; }
+
+                string company = input[0];
+                string employ = input[1];
+                if (!kvpCompany.ContainsKey(company))
+                {
+                    kvpCompany.Add(company, new List<string>());
+                    kvpCompany[company].Add(employ);
+                }
+                else
+                {
+                    if (!kvpCompany[company].Contains(employ)) { kvpCompany[company].Add(employ); }
+
+                }
+            }
         }
     }
 }
