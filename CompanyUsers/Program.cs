ae4531c6824707aecfd543e520659713d9f0b8f8
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-04 18:23:11 +0200

    Create Program.cs

---
+++ CompanyUsers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
