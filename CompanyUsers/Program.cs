30f0f86c1c1b212c3e965f749c2093dddc7df06f
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2023-11-05 10:25:08 +0200

    Update Program.cs

--- CompanyUsers/Program.cs
+++ CompanyUsers/Program.cs
@@ -13,16 +13,25 @@
                 if (input[0] == "End") { break; }
 
                 string company = input[0];
-                string employ = input[1];
+                string id = input[1];
                 if (!kvpCompany.ContainsKey(company))
                 {
                     kvpCompany.Add(company, new List<string>());
-                    kvpCompany[company].Add(employ);
+                    kvpCompany[company].Add(id);
                 }
                 else
                 {
-                    if (!kvpCompany[company].Contains(employ)) { kvpCompany[company].Add(employ); }
+                    if (!kvpCompany[company].Contains(id))
+                    { kvpCompany[company].Add(id); }
+                }
+            }
 
+            foreach (var company in kvpCompany)
+            {
+                Console.WriteLine($"{company.Key}");
+                foreach (var ids in company.Value)
+                {
+                    Console.WriteLine($"-- {ids}");
                 }
             }
         }
