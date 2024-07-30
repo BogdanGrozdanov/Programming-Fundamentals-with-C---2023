ca354a489b07f8619a2283fe0044e4c76837d858
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2024-07-30 10:28:59 +0300

    we

---
+++ AddAndSubstract/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+
+namespace AddAndSubstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            BigInteger command = BigInteger.Parse(Console.ReadLine());
+
+            for (BigInteger i = 1; i <= command; i += 1)
+            {
+                Console.Write(i + 1 + " ");
+            }
+
+
+
+            //while (command != "End")
+            //{
+
+            //    if (command == "+")
+            //    {
+            //        Console.Write("Enter a number 1 :");
+            //        int num1 = int.Parse(Console.ReadLine());
+            //        Console.Write("Enter a number 2 :");
+            //        int num2 = int.Parse(Console.ReadLine());
+            //        result = num1 + num2; //result
+            //    }
+            //    else if (command == "Bogdan")
+            //    {
+            //        Console.WriteLine("Is my dad");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Simona");
+            //    }
+            //    Console.WriteLine($"Result is : {result} ");
+            //    result = 0;
+            //    Console.Write("Enter a command :");
+            //    command = Console.ReadLine();
+            //}
+            //Console.WriteLine("Program ended!");
+        }
+    }
+}
---
+++ ZadachiZaKrisi/ZadachiZaKrisi.cs
@@ -0,0 +1,66 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+using System.Media;
+
+namespace ZadachiZaKrisi
+{
+    internal class ZadachiZaKrisi
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Ще решим ли няколко задачи???");
+            Console.WriteLine("Колко на брой да са?");
+            int start = int.Parse(Console.ReadLine());
+
+            int counterTrue = 0;
+            int counterFalse = 0;
+            Console.Write("Ще започваме ли?"); string startOrStop = Console.ReadLine();
+            while (startOrStop != "да".ToLower() || startOrStop != "da".ToLower())
+            {
+                for (int i = 1; i <= start; i++)
+                {
+                    Console.WriteLine($"Задача {i}");
+                    Console.WriteLine();
+
+                    Random rnd = new Random();
+                    int number1 = rnd.Next(1, 10);  //00
+                    int number2 = rnd.Next(1, 10);
+
+                    Console.Write($"{number1} + {number2} = ");
+                    int result = int.Parse(Console.ReadLine());
+
+                    if (result == number2 + number1)
+                    {
+                        Console.WriteLine($" Вярно!!!" + Environment.NewLine);
+
+                        Console.Beep(450, 650);
+
+                        counterTrue++;
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Грешно" + Environment.NewLine);
+                        Console.Beep(900,1000);
+                        counterFalse++;
+                    };
+                }
+                PrintResult(counterTrue, counterFalse);
+
+                Console.WriteLine("Да продължаваме ли?");
+                startOrStop = Console.ReadLine();
+            }
+            PrintResult(counterTrue, counterFalse);
+        }
+
+        private static void PrintResult(int counterTrue, int counterFalse)
+        {
+            Console.WriteLine("Твоят резултат е:");
+            Console.WriteLine($"Грешни отговори {counterFalse}");
+            Console.WriteLine($"Вярни отговори {counterTrue}" + Environment.NewLine);
+        }
+    }
+}
