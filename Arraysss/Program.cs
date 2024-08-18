afc94ab9c9701bc5e2da8fccb058755ff2e574b8
BogdanGrozdanov <b.grozdanov.tk@gmail.com>
2024-08-18 12:16:04 +0300

    ss

--- Arraysss/Program.cs
+++ Arraysss/Program.cs
@@ -4,40 +4,40 @@
     {
         static void Main(string[] args)
         {
-            //string[] weekdays ={ "Monday",
-            //                    "Tuesday",
-            //                    "Wednesday",
-            //                    "Thursday",
-            //                    "Friday",
-            //                    "Saturday",
-            //                    "Sunday" };
+            string[] weekdays ={ "Monday",
+                                "Tuesday",
+                                "Wednesday",
+                                "Thursday",
+                                "Friday",
+                                "Saturday",
+                                "Sunday" };
 
-            //int day = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
 
-            //if (day >= 1 && day <= 7)
-            //{
-            //    Console.WriteLine(weekdays[day - 1]);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Invalid day!");
-            //}
+            if (day >= 1 && day <= 7)
+            {
+                Console.WriteLine(weekdays[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
 
 
-            //int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
 
-            //int[] numbers = new int[n];
+            int[] numbers = new int[n];
 
-            //for (int i = 0; i < n; i++)
-            //{
-            //    int number = int.Parse(Console.ReadLine());
-            //    numbers[i] = number;
-            //}
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers[i] = number;
+            }
 
-            //for (int i = numbers.Length - 1; i >= 0; i--)
-            //{
-            //    Console.Write(numbers[i]+ " ");
-            //}
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
         }
     }
 }
